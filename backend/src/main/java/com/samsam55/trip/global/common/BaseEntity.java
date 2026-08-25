b4afa0f48ca1de57b6a399495277339bafa250b0
CHANUK HEO <siai3647@gmail.com>
2026-08-25 14:43:16 +0900

    [BE] feat: BaseEntity에서 식별자 분리

--- backend/src/main/java/com/samsam55/trip/global/common/BaseEntity.java
+++ backend/src/main/java/com/samsam55/trip/global/common/BaseEntity.java
@@ -2,9 +2,6 @@ package com.samsam55.trip.global.common;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -22,10 +19,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
