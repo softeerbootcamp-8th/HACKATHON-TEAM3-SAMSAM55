9a5f7c6d582808bc9c5203ce4566c03549cac56d
dlsnfl0615 <hyunseo0750@gmail.com>
2026-08-25 13:48:45 +0900

    [BE] chore: CrossOrigin 어노테이션 제거 배포 테스트

--- backend/src/main/java/com/samsam55/trip/echo/EchoController.java
+++ backend/src/main/java/com/samsam55/trip/echo/EchoController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 public class EchoController {
 
