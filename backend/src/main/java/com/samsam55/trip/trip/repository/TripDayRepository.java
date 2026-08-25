a39f50aab94fe969dbca778ee22e84b133dcb6a3
CHANUK HEO <siai3647@gmail.com>
2026-08-25 17:11:50 +0900

    [BE] feat: 여행 생성 API 구현

---
+++ backend/src/main/java/com/samsam55/trip/trip/repository/TripDayRepository.java
@@ -0,0 +1,7 @@
+package com.samsam55.trip.trip.repository;
+
+import com.samsam55.trip.trip.entity.TripDay;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripDayRepository extends JpaRepository<TripDay, Long> {
+}
