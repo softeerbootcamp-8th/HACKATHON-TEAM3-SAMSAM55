fd078fd3600fdf64a92edefbc79d0ff9081ed9d3
CHANUK HEO <siai3647@gmail.com>
2026-08-25 17:13:55 +0900

    [BE] feat: 여행 삭제 API 구현

--- backend/src/main/java/com/samsam55/trip/trip/repository/TripDayRepository.java
+++ backend/src/main/java/com/samsam55/trip/trip/repository/TripDayRepository.java
@@ -2,6 +2,13 @@ package com.samsam55.trip.trip.repository;
 
 import com.samsam55.trip.trip.entity.TripDay;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TripDayRepository extends JpaRepository<TripDay, Long> {
+
+    @Modifying(flushAutomatically = true)
+    @Query("delete from TripDay day where day.trip.id = :tripId")
+    int deleteAllByTripId(@Param("tripId") Long tripId);
 }
