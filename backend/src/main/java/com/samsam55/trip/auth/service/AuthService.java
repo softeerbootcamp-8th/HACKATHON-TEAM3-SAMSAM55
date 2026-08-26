18290274592d999c321493f1ab702f584f27d5bd
Woojae-Jeong <woo6388@naver.com>
2026-08-27 00:57:51 +0900

    [BE] style: 배포 CICD 테스트용 커밋

--- backend/src/main/java/com/samsam55/trip/auth/service/AuthService.java
+++ backend/src/main/java/com/samsam55/trip/auth/service/AuthService.java
@@ -65,6 +65,7 @@ public class AuthService {
             AuthLoginRequestDto request,
             HttpServletRequest servletRequest,
             HttpServletResponse servletResponse
+
     ) {
         User user = userRepository.findByLoginId(request.loginId())
                 .orElseThrow(() -> new ApplicationException(AuthErrorType.INVALID_CREDENTIALS));
